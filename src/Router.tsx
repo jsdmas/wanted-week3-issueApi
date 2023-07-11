b1f4d0ce42fafa937553099f953e2674f59052e3
jsdmas <sdmas5730@gmail.com>
2023-07-11 23:27:42 +0900

    :sparkles: Feat : issue page

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import { lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Home = lazy(() => import('./pages/home/index'));
+const Issue = lazy(() => import('./pages/issue/index'));
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/issue" element={<Issue />} />
       </Routes>
     </BrowserRouter>
   );
