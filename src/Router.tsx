d4fced1bf925305cce4d5698b0b1dc079a985817
jsdmas <sdmas5730@gmail.com>
2023-07-11 23:37:01 +0900

    :sparkles: Feat : suspense , Loading component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,7 @@
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Loading from './components/Loading';
 
 const Home = lazy(() => import('./pages/home/index'));
 const Issue = lazy(() => import('./pages/issue/index'));
@@ -8,11 +10,13 @@ const NotFound = lazy(() => import('./pages/notFound/index'));
 function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/issue" element={<Issue />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/issue" element={<Issue />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
