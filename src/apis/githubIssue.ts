ecafb2b6fa61e3296188050a2ba8315312b7b897
jsdmas <sdmas5730@gmail.com>
2023-07-11 23:17:19 +0900

    :sparkles: Test : get Github Issue

---
+++ src/apis/githubIssue.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.github.com/repos/';
+
+export const getGithubIssue = async () => {
+  // curl -L \
+  // -H "Authorization: Bearer process.env.REACT_APP_GITHUB_TOKEN"\
+  // -H "X-GitHub-Api-Version: 2022-11-28" \
+  // https://api.github.com/orgs/ORG/issues
+
+  // curl -L \
+  // -H "Accept: application/vnd.github+json" \
+  // -H "Authorization: Bearer <YOUR-TOKEN>"\
+  // -H "X-GitHub-Api-Version: 2022-11-28" \
+  // https://api.github.com/repos/OWNER/REPO/issues
+  const data = await axios.get(`${BASE_URL}facebook/react/issues`, {
+    headers: {
+      Accept: 'application/vnd.github+json',
+      Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      'X-GitHub-Api-Version': '2022-11-28',
+    },
+  });
+
+  console.log(data);
+
+  return data;
+};
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from 'react';
+import { getGithubIssue } from 'src/apis/githubIssue';
+
 function Home() {
+  useEffect(() => {
+    getGithubIssue();
+  }, []);
+
   return <div>Home</div>;
 }
 export default Home;
