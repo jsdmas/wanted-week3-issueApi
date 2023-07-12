d77ec18e9abcbb83d3cb0fdc93d15e759db763f7
jsdmas <sdmas5730@gmail.com>
2023-07-12 15:49:59 +0900

    :sparkles: Feat : infiniti scroll

--- src/apis/githubIssue.ts
+++ src/apis/githubIssue.ts
@@ -2,25 +2,31 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com/repos/';
 
-export const getGithubIssue = async () => {
-  // curl -L \
-  // -H "Authorization: Bearer process.env.REACT_APP_GITHUB_TOKEN"\
-  // -H "X-GitHub-Api-Version: 2022-11-28" \
-  // https://api.github.com/orgs/ORG/issues
-
+export const getGithubIssue = async (page: number) => {
   // curl -L \
   // -H "Accept: application/vnd.github+json" \
   // -H "Authorization: Bearer <YOUR-TOKEN>"\
   // -H "X-GitHub-Api-Version: 2022-11-28" \
   // https://api.github.com/repos/OWNER/REPO/issues
-  // ?sort=comment?state=open
-  const { data } = await axios.get(`${BASE_URL}facebook/react/issues?sort=comments&state=open`, {
+  // ?sort=comment&state=open&per_page=60
+
+  const queryObject = {
+    sort: 'comments',
+    state: 'open',
+    per_page: String(page),
+  };
+  const queryString = new URLSearchParams(queryObject).toString();
+
+  console.log(queryString);
+  const { data } = await axios.get(`${BASE_URL}facebook/react/issues?${queryString}`, {
     headers: {
       Accept: 'application/vnd.github+json',
       Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
       'X-GitHub-Api-Version': '2022-11-28',
     },
   });
+
+  console.log(data);
 
   return data;
 };
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { getGithubIssue } from 'src/apis/githubIssue';
 import Header from 'src/components/Header/Header';
 import Issue from 'src/components/Issue';
@@ -8,14 +8,36 @@ import { Iissue } from 'src/types/dataType';
 
 function Home() {
   const [data, setData] = useState([]);
+  const observerDiv = useRef<HTMLDivElement>(null);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
-    getGithubIssue().then((response) => setData(response));
-  }, [setData]);
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          // 타겟 요소와 루트 요소가 교차할시
+          if (entry.isIntersecting) {
+            getGithubIssue(page).then((response) => setData(response));
+            setPage((prev) => prev + 4);
+          }
+        });
+      },
+      { threshold: 0.5 },
+    );
+
+    if (observerDiv.current) {
+      observer.observe(observerDiv.current);
+    }
+  }, [page]);
+
+  // redux로 page number 관리
+  // contextAPi로 data넘겨주기
+  // intersectionObserver 설정
 
   return (
     <Layout header={<Header owner="facebook" repo="react" />}>
       <IssueList items={data}>{(item: Iissue) => <Issue key={item.id} {...item} />}</IssueList>
+      <div style={{ height: '20vh' }} ref={observerDiv} />
     </Layout>
   );
 }
