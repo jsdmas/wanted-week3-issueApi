92dd2998f08a94fe9a9a9fb81fc8d37dd050f2be
jsdmas <sdmas5730@gmail.com>
2023-07-14 14:09:27 +0900

    :fire: Remove : dumy store file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,11 @@
 import { combineReducers, createStore } from 'redux';
 
-import issueDataReducer from './issueData';
 import issuePageReducer from './issuePage';
 import loadDiscriminationReducer from './loading';
 
 const rootReducer = combineReducers({
   page: issuePageReducer,
   isLoading: loadDiscriminationReducer,
-  data: issueDataReducer,
 });
 
 export const store = createStore(rootReducer);
